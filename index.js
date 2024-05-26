1ff661f4492e7ac6e1524a8f107e9c74cbb1e39f
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:13:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Day 03
+// Question 07
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+// Question 08
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+// Question 09
+let favNumber = 8;
+console.log(`${favNumber} is my favourite number.`);
